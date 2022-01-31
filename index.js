34c39cbd77440099e2629496696bee7e1b6d976f
Jamie Bunch <JamieBunch@ymail.com>
2022-01-31 12:38:44 -0500

    dataArrays init

---
+++ index.js
@@ -0,0 +1,26 @@
+//1-Write a JavaScript function to get the first element of an array
+// let array = [3, 6, 9, 12, 8, 2]
+
+// function array1(arr1){
+//     console.log(arr1[0])
+// }  
+// array1(array)
+
+//2-Write a JavaScript function to get the last element of an array
+// let array = [3, 6, 9, 12, 8, 2]
+
+// function array1(arr1){
+//     console.log(arr1[5])
+// }  
+// array1(array)
+
+//3-Write a JavaScript function to get a given element of an array with the index passed as a parameter
+// const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
+
+// console.log(beasts.indexOf('bison', 2));
+// expected output: 4
+
+//4-Write a JavaScript function to get the index of an element in an array where it's element matches a value passed as a parameter
+
+let array = [3, 6, 9, 12, 8, 2]
+function find index()
